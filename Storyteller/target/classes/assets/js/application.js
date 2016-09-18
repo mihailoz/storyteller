97edf9aedac74e5f944a244fe600e7405f58815d
Mihailo Zdravkovic <mihailo.zdravkovic@digitaz.rs>
2016-09-18 20:28:34 +0200

    Created server-side game code

--- Storyteller/target/classes/assets/js/application.js
+++ Storyteller/target/classes/assets/js/application.js
@@ -1,46 +1,73 @@
 $(function() {
-    var playerId;
-
-    var startGame = function() {
-        var socket = atmosphere;
-
-        console.log(socket);
-        var request = {
-            url: document.location.toString() + 'play/' + playerId,
-            contentType: "application/json",
-            logLevel: 'debug',
-            transport: 'websocket',
-            fallbackTransport: 'long-polling'
-        };
-
-        request.onOpen = function(response) {
-            console.log(response);
-        };
-
-        request.onMessage = function(response) {
-            console.log(response);
-        };
-
-        var subSocket = socket.subscribe(request);
-
-        $('#nekiInput').keydown(function(e) {
-            if (e.keyCode === 13) {
-                var msg = $(this).val();
-
-                subSocket.push(msg);
-                $(this).val('');
+    var playerId, gameActive = true;
+    
+    var checkStatus = function () {
+        $.ajax({
+            url: "./play/status/" + playerId,
+            dataType: "text",
+            success: function (data) {
+                var response = JSON.parse(data);
+                
+                if(response.status.string === "queueing") {
+                    setTimeout(function () {
+                        checkStatus();
+                    }, 2000);
+                } else if(response.status.string === "inGame") {
+                    if(response.onTurn.string === "true") {
+                        $("#submit").prop("disabled", false);
+                        console.log("Your turn :D");
+                        $("#storyParagraph").text(response.story.string);
+                        setTimeout(function () {
+                            checkStatus();
+                            $("#submit").prop("disabled", true);
+                        }, 8000);
+                    } else {
+                        $("#submit").prop("disabled", true);
+                        $("#storyParagraph").text(response.story.string);
+                        setTimeout(function () {
+                            checkStatus();
+                        }, 2000);
+                    }
+                    
+                }
             }
         });
+    }
+    
+    $('#submit').on('click', function (e) {
+        var word = $("#nekiInput").val();
+        $.ajax({
+            type: "POST",
+            url: "./play/turn/" + playerId + "/" + word,
+            data: {},
+            success: function (data) {
+                $("#submit").prop("disabled", true);
+                checkStatus();
+            }
+        });
+    });
+    
+    var startGame = function() {
+        checkStatus();
     };
-
+    
     $("#findGame").click(function() {
         $.ajax({
-            url: "./api",
+            url: "./play",
             dataType: "text",
             success: function(data) {
                 playerId = data;
                 startGame();
             }
         });
+        
+        window.onbeforeunload = function () {
+           $.ajax({
+               type: "POST",
+               url: "./play/leave/" + playerId,
+               data: {}
+           }); 
+        };
     });
+    
 });
