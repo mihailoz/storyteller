a1558ad0033f668cfcf497f39ae782e8a8823c7c
skrbica <marko.ns.skrbic@gmail.com>
2016-09-26 13:02:50 +0200

    dugmici

--- Storyteller/src/main/resources/assets/js/application.js
+++ Storyteller/src/main/resources/assets/js/application.js
@@ -1,29 +1,30 @@
 $(function() {
     var playerId, gameActive = true;
 
-    var checkStatus = function () {
+    var checkStatus = function() {
         $.ajax({
             url: "./play/status/" + playerId,
             dataType: "text",
-            success: function (data) {
+            success: function(data) {
                 var response = JSON.parse(data);
 
-                if(response.status.string === "queueing") {
+                if (response.status.string === "queueing") {
                     // If in queue
                     $("#storyParagraph").text("Queueing, please wait...");
 
-                    setTimeout(function () {
+                    setTimeout(function() {
                         checkStatus();
                     }, 2000);
-                } else if(response.status.string === "inGame") {
+                } else if (response.status.string === "inGame") {
                     // If in game
 
-                    if(response.onTurn.string === "true") {
+                    if (response.onTurn.string === "true") {
                         // If on turn
                         $("#submitButton").prop("disabled", false);
-
+                        $("#userInput").prop("disabled", false);
+                        $("#userInput").focus();
                         $("#storyParagraph").text(response.story.string);
-                        setTimeout(function () {
+                        setTimeout(function() {
                             checkStatus();
                             $("#submitButton").prop("disabled", true);
                         }, 8000);
@@ -31,7 +32,7 @@ $(function() {
                         $("#submitButton").prop("disabled", true);
                         $("#userInput").prop("disabled", true);
                         $("#storyParagraph").text(response.story.string);
-                        setTimeout(function () {
+                        setTimeout(function() {
                             checkStatus();
                         }, 2000);
                     }
@@ -41,13 +42,13 @@ $(function() {
         });
     }
 
-    $('#submitButton').on('click', function (e) {
+    $('#submitButton').on('click', function(e) {
         var word = $("#userInput").val();
         $.ajax({
             type: "POST",
             url: "./play/turn/" + playerId + "/" + word,
             data: {},
-            success: function (data) {
+            success: function(data) {
                 $("#submitButton").prop("disabled", true);
                 checkStatus();
             }
@@ -70,12 +71,12 @@ $(function() {
         }
     });
 
-    window.onbeforeunload = function () {
-       $.ajax({
-           type: "POST",
-           url: "./play/leave/" + playerId,
-           data: {}
-       });
+    window.onbeforeunload = function() {
+        $.ajax({
+            type: "POST",
+            url: "./play/leave/" + playerId,
+            data: {}
+        });
     };
 
 });
