641c238805813dc824b333b4d24c680112ed6d78
Mihailo Zdravkovic <mihailo.zdravkovic@digitaz.rs>
2016-09-30 20:42:06 +0200

    Updated application.js

--- Storyteller/src/main/resources/assets/js/application.js
+++ Storyteller/src/main/resources/assets/js/application.js
@@ -115,7 +115,7 @@ $(function() {
     });
 
     $("#pollYes").on('click', function() {
-        $.ajax({
+        $.ajax({    
             type: "POST",
             url: "./play/poll/" + playerId + "/endGame",
             data: {},
