56238f74a308455901d9dd87d9afab558a6c1bae
skrbica <marko.ns.skrbic@gmail.com>
2016-10-09 12:30:10 +0200

    join modal updated

--- Storyteller/src/main/resources/assets/js/shareGame.js
+++ Storyteller/src/main/resources/assets/js/shareGame.js
@@ -2,7 +2,7 @@ $(function () {
     var gameId = window.location.search;
     gameId = gameId.substring(1, gameId.length);
     $.ajax({
-        url: "./api/history/" + gameId,
+        url: "./play/history/" + gameId,
         dataType: "text",
         success: function(data) {
             $('#storyParagraph').text(data);
